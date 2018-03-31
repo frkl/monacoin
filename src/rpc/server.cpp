9d65cf396c7170b645fdcce1077dbcc0c8cd75a7
frkl <name@example.org>
2018-03-31 08:38:07 -0400

    getwork

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -218,7 +218,7 @@ UniValue help(const JSONRPCRequest& jsonRequest)
 {
     if (jsonRequest.fHelp || jsonRequest.params.size() > 1)
         throw std::runtime_error(
-            "help ( \"command\" )\n"
+            "monacoin help ( \"command\" )\n"
             "\nList all commands, or get help for a specified command.\n"
             "\nArguments:\n"
             "1. \"command\"     (string, optional) The command to get help on\n"
